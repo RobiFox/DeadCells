48fb7f76d7b1ced6838723db0fbafd085b10aa20
Robieh <10639273+RobiFox@users.noreply.github.com>
2024-08-26 00:00:35 +0200

    added leaderlines

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Inject, ViewChild} from '@angular/core';
 import {SettingsComponent} from "../settings/settings.component";
 import {CheckboxModule} from "primeng/checkbox";
 import {DropdownModule} from "primeng/dropdown";
 import {NgOptimizedImage} from "@angular/common";
+
+declare var LeaderLine: any;
 
 @Component({
   selector: 'app-map',
@@ -16,6 +18,24 @@ import {NgOptimizedImage} from "@angular/common";
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
-export class MapComponent {
+export class MapComponent implements AfterViewInit {
+  @ViewChild('a1') a1!: ElementRef;
+  @ViewChild('b1') b1!: ElementRef;
+  @ViewChild('b2') b2!: ElementRef;
+  @ViewChild('b3') b3!: ElementRef;
+  @ViewChild('c1') c1!: ElementRef;
+  @ViewChild('c2') c2!: ElementRef;
+  ngAfterViewInit() {
+    this.createLine(this.a1.nativeElement, this.b1.nativeElement);
+    this.createLine(this.a1.nativeElement, this.b2.nativeElement);
+    this.createLine(this.b1.nativeElement, this.c1.nativeElement);
+    this.createLine(this.b2.nativeElement, this.c2.nativeElement);
+  }
 
+  createLine(a: any, b: any) {
+    if (typeof LeaderLine === 'undefined') return;
+    const line = new LeaderLine(a, b);
+    line.path = "grid";
+    line.color = "green";
+  }
 }
